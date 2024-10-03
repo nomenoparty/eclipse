b70f5692b73bf721c4f4288e9b886d5c3805bbe3
nomenoparty <khronal777@gmail.com>
2024-10-03 11:24:21 +0700

    test11

--- src/com/helloworld/HelloServlet.java
+++ src/com/helloworld/HelloServlet.java
@@ -61,7 +61,7 @@ public class HelloServlet extends HttpServlet {
 		ArrayList<Integer> list = new ArrayList<>();
 		list.add(2);
 		list.add(4);
-		list.add(1);
+		list.add(10);
 
 		req.setAttribute("collection", list);
 
